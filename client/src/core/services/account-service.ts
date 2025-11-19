07ed2ca8be017c6090ba22f3c40b9117208e8807
MadsAkselsen <akselsenmads@gmail.com>
2025-11-19 13:58:53 +0100

    get roles in frontend

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -3,47 +3,55 @@ import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment';
-import {LikesService} from './likes-service';
+import { LikesService } from './likes-service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AccountService {
-  private http = inject(HttpClient);
-  private likesService = inject(LikesService);
-  currentUser = signal<User | null>(null);
-  private baseUrl = environment.apiUrl;
+	private http = inject(HttpClient);
+	private likesService = inject(LikesService);
+	currentUser = signal<User | null>(null);
+	private baseUrl = environment.apiUrl;
 
-  register(creds: RegisterCreds) {
-    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
-      tap(user => {
-        if (user) {
-          this.setCurrentUser(user);
-        }
-      })
-    );
-  }
+	register(creds: RegisterCreds) {
+		return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+			tap(user => {
+				if (user) {
+					this.setCurrentUser(user);
+				}
+			})
+		);
+	}
 
-  login(creds: LoginCreds) {
-    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
-      tap(user => {
-        if (user) {
-          this.setCurrentUser(user);
-        }
-      })
-    );
-  }
+	login(creds: LoginCreds) {
+		return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+			tap(user => {
+				if (user) {
+					this.setCurrentUser(user);
+				}
+			})
+		);
+	}
 
-  setCurrentUser(user: User) {
-    localStorage.setItem('user', JSON.stringify(user));
-    this.currentUser.set(user);
-    this.likesService.getLikeIds();
-  }
+	setCurrentUser(user: User) {
+		user.roles = this.getRolesFromToken(user);
+		localStorage.setItem('user', JSON.stringify(user));
+		this.currentUser.set(user);
+		this.likesService.getLikeIds();
+	}
 
-  logout() {
-    localStorage.removeItem('user');
-    localStorage.removeItem('filters');
-    this.likesService.clearLikeIds();
-    this.currentUser.set(null);
-  }
+	logout() {
+		localStorage.removeItem('user');
+		localStorage.removeItem('filters');
+		this.likesService.clearLikeIds();
+		this.currentUser.set(null);
+	}
+
+	private getRolesFromToken(user: User): string[] {
+		const payload = user.token.split('.')[1]; // base64 string
+		const decoded = atob(payload); // atob is decoding base64 string
+		const jsonPayload = JSON.parse(decoded);
+		return Array.isArray(jsonPayload.role) ? jsonPayload.role : [jsonPayload.role];
+	}
 }
