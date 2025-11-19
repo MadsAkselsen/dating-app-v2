3529be964b32543703e2c7dc6bce0616b52b51dc
MadsAkselsen <akselsenmads@gmail.com>
2025-11-19 06:43:48 +0100

    create new identity migration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -24,8 +24,7 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         {
             DisplayName = registerDto.DisplayName,
             Email = registerDto.Email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key,
+            UserName = registerDto.Email,
             Member = new Member
             {
                 DisplayName = registerDto.DisplayName,
@@ -48,20 +47,12 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
         
         if (user == null) throw new UnauthorizedException("Invalid email address");
-        
-        using var hmac = new HMACSHA512(user.PasswordSalt);
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-        for (var i = 0; i < computedHash.Length; i++)
-        {
-            if (computedHash[i] != user.PasswordHash[i]) throw new UnauthorizedException("Invalid password");
-        }
 
         return user.ToDto(tokenService);
     }
 
     private async Task<bool> EmailExists(string email)
     {
-        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        return await context.Users.AnyAsync(x => x.Email!.ToLower() == email.ToLower());
     }
 }
